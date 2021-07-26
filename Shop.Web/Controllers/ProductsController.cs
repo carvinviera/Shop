e75a5394c18d38f90bd5cc6a0df7b7b2ed5b3fb5
Carvin Viera <cjvs_carvinviera@hotmail.com>
2021-07-26 19:55:08 -0300

    Add Imagenes en Create

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -5,6 +5,8 @@
     using Shop.Web.Data;
     using Shop.Web.Data.Entities;
     using Shop.Web.Helper;
+    using Shop.Web.Models;
+    using System.IO;
     using System.Threading.Tasks;
 
     public class ProductsController : Controller
@@ -53,17 +55,53 @@
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Product product)
+        public async Task<IActionResult> Create(ProducViewModel view)
         {
             if (ModelState.IsValid)
             {
+                var path = string.Empty; 
+
+                if (view.ImageFile != null && view.ImageFile.Length > 0)
+                {
+                    path = Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "wwwroot\\images\\Products",
+                        view.ImageFile.FileName);
+
+                    using (var stream= new FileStream(path, FileMode.Create))
+                    {
+                        await view.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                }
+
+                var product = this.toProduct(view, path);
+
                 //TODO: Change for the logger user
                 product.User = await userHelper.GetUserByMailAsync("carvin.viera@osdop.org.ar");
                 await this.productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(view);
         }
+
+        private Product toProduct(ProducViewModel view, string path)
+        {
+            return new Product
+            {
+                Id = view.Id,
+                ImageUrl = path,
+                IsAvailabe = view.IsAvailabe,
+                LastPurchase = view.LastPurchase,
+                LastSale = view.LastSale,
+                Name = view.Name,
+                Price = view.Price,
+                Stock = view.Stock,
+                User = view.User
+            };
+        }
+
 
         // GET: Products/Edit/5
         public async Task<IActionResult> Edit(int? id)
